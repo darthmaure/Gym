9afd4cd0dbebe916b19437b20a5042aaed62e772
pawelk <darth.maure@hotmail.com>
2017-11-16 11:23:24 +0100

    Lib for settings. Images folder, some improvements

---
+++ Gym/Gym/Gym/Services/SettingsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gym.Models;
+using Plugin.Settings;
+
+namespace Gym.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        public SettingsEntry Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(SettingsEntry entry)
+        {
+            throw new NotImplementedException();
+            //AppSettings.AddOrUpdateValue(DailyLimtSettingsKey, value);
+            //AppSettings.AddOrUpdateValue(MonthlyLimitSettingsKey, value);
+        }
+    }
+}
